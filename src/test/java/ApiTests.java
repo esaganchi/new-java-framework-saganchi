7412ba811958ffea0715db793971d812c6ddf0dc
Evgenii Saganchi <esaganchi@gmail.com>
2025-08-19 23:21:38 +0200

    Constants Edding

--- src/test/java/ApiTests.java
+++ src/test/java/ApiTests.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
+import static testdata.TestData.DEFAULT_USER;
+import static testdata.TestData.INVALID_USER;
 
 class ApiTests {
     UserController userController = new UserController();
@@ -71,24 +73,18 @@ class ApiTests {
 
     @Test
     void createUserControllerTest() {
-        User user = new User(0,
-                "username",
-                "firstName",
-                "lastName",
-                "email",
-                "password",
-                "phone",
-                0);
-        User userBuilder = User.builder()
-                .username("username")
-                .firstName("firstName")
-                .lastName("lastName")
-                .email("email")
-                .phone("password")
-                .userStatus(0)
-                .build();
+        Response response = userController.createUser(DEFAULT_USER);
+        AddUserResponse createdUserResponse = response.as(AddUserResponse.class);
 
-        Response response = userController.createUser(user);
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals(200, createdUserResponse.getCode());
+        Assertions.assertEquals("unknown", createdUserResponse.getType());
+        Assertions.assertFalse(createdUserResponse.getMessage().isEmpty());
+    }
+
+    @Test
+    void createUserControllerTest2() {
+        Response response = userController.createUser(INVALID_USER);
         AddUserResponse createdUserResponse = response.as(AddUserResponse.class);
 
         Assertions.assertEquals(200, response.statusCode());
@@ -97,78 +93,3 @@ class ApiTests {
         Assertions.assertFalse(createdUserResponse.getMessage().isEmpty());
     }
 }
-
-
-
-//import io.restassured.response.Response;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//
-//import static io.restassured.RestAssured.given;
-//import static org.hamcrest.Matchers.equalTo;
-//import static org.hamcrest.Matchers.notNullValue;
-//
-//public class SmokeApiTests {
-//    @Test
-//    void createUser() {
-//        String baseUrl = "https://petstore.swagger.io/v2/";
-//        String body = """
-//                {
-//                  "id": 0,
-//                  "username": "string",
-//                  "firstName": "string",
-//                  "lastName": "string",
-//                  "email": "string",
-//                  "password": "string",
-//                  "phone": "string",
-//                  "userStatus": 0
-//                }""";
-//
-//        Response response =
-//                given()
-//                    .baseUri(baseUrl)
-//                    .header("accept", "application/json")
-//                    .header("Content-Type", "application/json")
-//                    .body(body).
-//                when()
-//                    .post("user")
-//                    .andReturn();
-//        response.body().prettyPrint();
-//
-//        int actualCode = response.getStatusCode();
-//
-//        Assertions.assertEquals(200, actualCode);
-//    }
-//
-//    @Test
-//    void checkUserResponseBody() {
-//        String baseUrl = "https://petstore.swagger.io/v2/";
-//        String body = """
-//           {
-//             "id": 0,
-//             "username": "string",
-//             "firstName": "string",
-//             "lastName": "string",
-//             "email": "string",
-//             "password": "string",
-//             "phone": "string",
-//             "userStatus": 0
-//           }""";
-//
-//
-//        given()
-//                    .baseUri(baseUrl)
-//                    .header("accept", "application/json")
-//                    .header("Content-Type", "application/json")
-//                    .body(body).
-//                when()
-//                    .post("user")
-//                .then()
-//                    .statusCode(200)
-//                    .body("code", equalTo(200))
-//                    .body("type", equalTo("unknown"))
-//                    .body("message", notNullValue(String.class));
-//    }
-//}
-//
-//
--- src/test/java/models/User.java
+++ src/test/java/models/User.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor      // добавь это
+@NoArgsConstructor
 @Builder
 public class User {
     private int id;
