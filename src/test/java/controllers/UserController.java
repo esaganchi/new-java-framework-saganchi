1c1e9f63411896a149715e85cddd8fc0c12dd9ca
Evgenii Saganchi <esaganchi@gmail.com>
2025-08-20 23:30:12 +0200

    fix-properties

--- src/test/java/controllers/UserController.java
+++ src/test/java/controllers/UserController.java
@@ -1,51 +1,53 @@
 package controllers;
 
+import config.TestPropertiesConfig;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import models.User;
+import org.aeonbits.owner.ConfigFactory;
 
-import static constants.CommonConstants.BASE_URI;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 
 public class UserController {
     RequestSpecification requestSpecification;
     private static final String USER_ENDPOINT = "user";
+    TestPropertiesConfig configProperties = ConfigFactory.create(TestPropertiesConfig.class, System.getProperties());
 
     public UserController() {
         this.requestSpecification = given()
                 .accept(JSON)
                 .contentType(JSON)
-                .baseUri(BASE_URI);
+                .baseUri(configProperties.getApiBaseUrl());
     }
 
     public Response createUser(User user) {
         return given(this.requestSpecification)
-                    .body(user)
+                .body(user)
                 .when()
-                  .post(USER_ENDPOINT)
-                  .andReturn();
+                .post(USER_ENDPOINT)
+                .andReturn();
     }
 
     public Response updateUser(User user) {
         return given(this.requestSpecification)
-                   .body(user)
+                .body(user)
                 .when()
-                  .put(USER_ENDPOINT + "/" + user.getUsername())
-                  .andReturn();
+                .put(USER_ENDPOINT + "/" + user.getUsername())
+                .andReturn();
     }
 
     public Response getUserByUsername(String username) {
         return given(this.requestSpecification)
                 .when()
-                 .get(USER_ENDPOINT + "/" + username)
-                  .andReturn();
+                .get(USER_ENDPOINT + "/" + username)
+                .andReturn();
     }
 
     public Response deleteUserByUsername(String username) {
         return given(this.requestSpecification)
                 .when()
-                    .delete(USER_ENDPOINT + "/" + username)
-                    .andReturn();
+                .delete(USER_ENDPOINT + "/" + username)
+                .andReturn();
     }
 }
